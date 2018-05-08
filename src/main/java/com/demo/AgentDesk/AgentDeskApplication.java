be4985e452de3beff6a8e8698d5ab342ac28cfa0
Ashutosh Pandey <ashutoshpandey@INKACTXM023.local>
2018-05-08 21:13:33 +0530

    Pushing First commit

---
+++ src/main/java/com/demo/AgentDesk/AgentDeskApplication.java
@@ -0,0 +1,12 @@
+package com.demo.AgentDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentDeskApplication.class, args);
+	}
+}
